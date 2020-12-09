77031887e0c017e9e067574029d08630574b0431
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-08 22:37:49 -0700

    Add customer, dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
