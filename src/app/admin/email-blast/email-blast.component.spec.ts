bef11f38394f39d7c19edbc12627b770017572de
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-08 22:41:53 -0700

    admin routing

---
+++ src/app/admin/email-blast/email-blast.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmailBlastComponent } from './email-blast.component';
+
+describe('EmailBlastComponent', () => {
+  let component: EmailBlastComponent;
+  let fixture: ComponentFixture<EmailBlastComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EmailBlastComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EmailBlastComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
