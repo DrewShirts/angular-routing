70e8ca77444cf50f645b2880f9eabb2faf2fabf7
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-09 18:53:26 -0700

    e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('angular-routing app is running!');
+    expect(await page.getTitleText()).toEqual('Dashboard');
   });
 
   afterEach(async () => {
