10593e8e18de4585b205811743f262d9b3837f93
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-09 22:16:26 -0700

    generating libraries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoggerService } from 'my-lib';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-routing';
+
+  constructor(logger: LoggerService) {
+    logger.log('hello world');
+  }
 }
